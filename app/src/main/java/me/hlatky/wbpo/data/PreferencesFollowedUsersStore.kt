768eade31c99222ef300edc8dabfbc656ec967f9
Matej Hlatký <hlatky@freevision.sk>
2023-04-21 16:24:43 +0200

    Simplify store impl.

--- app/src/main/java/me/hlatky/wbpo/data/PreferencesFollowedUsersStore.kt
+++ app/src/main/java/me/hlatky/wbpo/data/PreferencesFollowedUsersStore.kt
@@ -4,40 +4,35 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringSetPreferencesKey
-import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.map
 
+/** [FollowedUsersStore] implementation that uses [DataStore]. */
 class PreferencesFollowedUsersStore(
     private val store: DataStore<Preferences>
 ) : FollowedUsersStore {
 
     override suspend fun getIsFollowed(userId: Int): Boolean {
         return store.data.map { prefs ->
-            prefs[KEY]?.contains(userId.toString()) ?: false
-        }.first()
+            prefs[KEY]?.contains(userId.toString())
+        }.firstOrNull() ?: false
     }
 
     override suspend fun followUser(userId: Int) {
-        store.edit { prefs ->
-            // TODO Extract into private fun with block
-            val newSet = prefs[KEY].orEmpty().toMutableSet().also {
-                it.add(userId.toString())
-            }
-
-            prefs[KEY] = newSet
-        }
+        modifyUserFollow { it.add(userId.toString()) }
     }
 
     override suspend fun unFollowUser(userId: Int) {
+        modifyUserFollow { it.remove(userId.toString()) }
+    }
+
+    private suspend fun modifyUserFollow(block: (set: MutableSet<String>) -> Unit) {
         store.edit { prefs ->
-            val newSet = prefs[KEY].orEmpty().toMutableSet().also {
-                it.remove(userId.toString())
-            }
+            val newSet = prefs[KEY].orEmpty().toMutableSet().also(block)
 
             prefs[KEY] = newSet
         }
     }
-
 
     companion object {
         private val KEY = stringSetPreferencesKey("followed_user_ids")
