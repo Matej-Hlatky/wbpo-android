1f4dd821b0eb90955494141df4a6ee0686f9f9b4
Matej Hlatký <hlatky@freevision.sk>
2023-04-21 11:11:55 +0200

    Add sample Fragment Android app from template
    Gradle 7.6.1
    AGP 7.4.2
    Kotlin 1.8
    SDK 24 - 33

---
+++ app/src/main/java/me/hlatky/wbpo/MainActivity.kt
@@ -0,0 +1,18 @@
+package me.hlatky.wbpo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import me.hlatky.wbpo.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/me/hlatky/wbpo/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package me.hlatky.wbpo.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
